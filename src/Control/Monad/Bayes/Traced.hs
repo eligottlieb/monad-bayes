2a860e9871181fb64c8ba67454482858dc4d2db5
esennesh <elisennesh@gmail.com>
2020-11-20 12:17:48 -0500

    Control.Monad.Bayes.Traced: category of weighted importance samplers template
    Signed-off-by: esennesh <elisennesh@gmail.com>

--- src/Control/Monad/Bayes/Traced.hs
+++ src/Control/Monad/Bayes/Traced.hs
@@ -13,4 +13,31 @@ module Control.Monad.Bayes.Traced (
   module Control.Monad.Bayes.Traced.Static
 ) where
 
+import Control.Category
 import Control.Monad.Bayes.Traced.Static
+import Numeric.Log (Log)
+
+type Trace = ([Double], Log Double)
+
+extend :: Trace -> Trace -> Trace
+extend (va, da) (vb, db) = (va ++ vb, da * db)
+
+data Learner p a b = Learner {
+  parameters :: p,
+  implementation :: p -> a -> b,
+  update :: p -> a -> b -> p,
+  request :: p -> a -> b -> a
+}
+
+instance Category (Learner Trace) where
+  id = Learner {parameters = ([], 1), implementation = \t a -> a,
+                update = \t a b -> t, request = \t a b -> b}
+  cb . ca = Learner {
+    parameters = extend (parameters ca) (parameters cb),
+    implementation = \t a -> implementation cb t $ implementation ca t a,
+    update = \t a c -> let b = implementation ca t a
+                           b' = request cb t b c in
+      extend (update ca t a b') (update cb t b c),
+    request = \t a c -> let b = implementation ca t a in
+      request ca t a $ request cb t b c
+  }
